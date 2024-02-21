818bb3fb1a6b179ea57b47c439e1130458f2543c
Abhishek Naula <94128925+Abro0058T@users.noreply.github.com>
2024-02-21 18:17:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const PORT=9000
 Connection();
 const io=new Server(PORT,{
     cors:{
-        origin:'http://localhost:3000',
+        origin:'https://google-doc-qbd7.vercel.app',
         methods:['GET','POST']
     }
 });
